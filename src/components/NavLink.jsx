b3e4be8418f20e61dc82356a480174c0bbc7bf76
Md Siyam <mdsiyam9424@gmail.com>
2026-07-15 13:01:49 +0600

    project setup and navbar completed

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,12 @@
+'use client'
+import { usePathname } from 'next/navigation';
+import Link from "next/link";
+
+export default function NavLink({href, children}) {
+    const pathname = usePathname();
+    const isActive = pathname === href;
+  return (
+    <Link href={href} className={isActive ? "border-b-2 border-teal-600 pb-0.5 " : " hover:text-teal-600 "
+        }>{children}</Link>
+  )
+}
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,49 @@
+
+import NavLink from '../NavLink';
+import Link from 'next/link';
+
+const Navbar = () => {
+ 
+  const links = <>
+    <li><NavLink href={"/"}>Home</NavLink></li>
+    <li><NavLink href={"/products"}>Products</NavLink></li>
+    <li><NavLink href={"/blog"}>Blog</NavLink></li>
+    <li><NavLink href={"/men"}>Men's</NavLink></li>
+    <li><NavLink href={"/women"}>Women's</NavLink></li>
+  </>
+
+  return (
+    <div>
+      <div className="navbar max-w-screen-2xl mx-auto fixed top-0 z-50 justify-between py-4 lg:px-20 bg-white/60 shadow-sm">
+        <div className="flex ">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              {
+                links
+              }
+            </ul>
+          </div>
+          <Link href={'/'} className="btn btn-ghost text-2xl  ">           
+            LiLy Fashion
+          </Link>
+        </div>
+        <div className="flex gap-24">
+          <div className="navbar-center hidden lg:flex">
+            <ul className="flex gap-8  menu-horizontal px-1">
+              {
+                links
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
